56a00e481c6dd7bba8c6ec8713bfef53aa7580d9
DrSumner <157843168+DrSumner@users.noreply.github.com>
2024-04-17 21:53:26 -0400

    finito

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,20 +1,115 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import Character from './Character'
 
 const urlPlanets = 'http://localhost:9009/api/planets'
 const urlPeople = 'http://localhost:9009/api/people'
 
-function App() {
+function  App() {
   // ❗ Create state to hold the data from the API
   // ❗ Create effects to fetch the data and put it in state
+  const [planetData, setplanetData] = useState(null)
+  const [peopleData, setpeopleData] = useState(null)
+useEffect(() => {
+
+
+  axios
+.get(urlPlanets)
+.then( res =>{
+  setplanetData(res.data)
+  console.log(res.data)
+})
+.catch( err =>
+console.log(err.message)
+)
+
+axios
+.get(urlPeople)
+.then( res =>{
+  console.log(res.data);
+  setpeopleData(res.data)
+})
+.catch( err =>
+console.log(err.message)
+)
+
+
+}, [])
+
+
+//  if(peopleData){
+//   console.log(peopleData[0])
+//   console.log(planetData.length)
+//   console.log(typeof planetData)
+//   console.log(typeof peopleData)
+//   console.log(peopleData[0])
+//  }
+
+
+
+
+ const findPlanet = data => {
+for (let i = 0; i< planetData.length; i++){
+  
+  if(planetData[i].id == data.homeworld){
+    return planetData[i].name
+  }
+}
+ }
+
+ if(peopleData){
+  peopleData.map(people =>
+    console.log(findPlanet(people))
+  )
+}
+
+
+
+
+ //if(peopleData && planetData){console.log( findPlanet(peopleData[0]))}
+
+ 
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
       {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      {peopleData ? (peopleData.map(people => 
+         
+        <Character
+        key = {people.id}
+        name = {people.name}
+        planet = {findPlanet(people)}
+        
+        />
+      
+      
+      )) :
+      <div> loading</div>
+      }
     </div>
   )
+
+
+
+  // return (
+  //   <>
+  //     {peopleData ? (
+  //       <div>
+  //         {peopleData.map( (people, index) => (
+  //           <div key={index}>
+  //             { findPlanet(people)}
+  //           </div>
+  //         ))}
+  //       </div>
+  //     ) : (
+  //       <p>Loading...</p>
+  //     )}
+  //   </>
+  // );
+
+
+
 }
 
 export default App
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function Character() { // ❗ Add the props
+function Character(props) { // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
   // ❗ Create a "toggle" click handler to show or remove the homeworld
+  const [isHomeRender, setHomeRender] = useState(false)
+  const {name, planet} = props
   return (
-    <div>
+    <div onClick={ () => setHomeRender(!isHomeRender) } className='character-card'>
       {/* Use the same markup with the same attributes as in the mock */}
+      <h3 className='character-name'>
+      {name}
+      </h3>
+      {isHomeRender && 
+      <p>Planet:{' '}
+        <span className='character-planet'>{planet}</span>
+      </p>}
+      
     </div>
   )
 }
